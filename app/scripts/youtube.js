b10af782c06a8667498f44780c428575f1cbbe80
CheAle14 <12370876+CheAle14@users.noreply.github.com>
2023-09-13 23:26:44 +0100

    fix typo

--- app/scripts/youtube.js
+++ app/scripts/youtube.js
@@ -750,7 +750,7 @@ function isDivThumbnail(div) {
     return div.tagName === "DIV" && div.id === "content"
 }
 function isYtdGridThumbnail(el) {
-    return el.tagName === "YTD-THUMBNAIL" || el.tagName === "YTD-ITEM-SECTION-RENDERER";
+    return el.tagName === "YTD-THUMBNAIL" || el.tagName === "YTM-ITEM-SECTION-RENDERER";
 }
 
 function storeThumbnailElement(id, element) {
